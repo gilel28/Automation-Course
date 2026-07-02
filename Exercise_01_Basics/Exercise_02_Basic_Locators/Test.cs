00a11a8d70bdb723b66a30ae1a53dd9cdc71693b
Gil Eliav <gil.eliav3@gmail.com>
2026-07-02 13:24:23 +0300

    add exercise 02

---
+++ Exercise_01_Basics/Exercise_02_Basic_Locators/Test.cs
@@ -0,0 +1,42 @@
+﻿using OpenQA.Selenium;
+
+namespace Exercise_02_Locators
+{
+    public class Test : BaseTest
+    {
+ 
+
+        [Test]
+        public void Test_01()
+        {
+
+            IWebElement locatorId = driver.FindElement(By.Id("locator_id"));
+            IWebElement locatorName = driver.FindElement(By.Name("locator_name"));
+            IWebElement locatorTag = driver.FindElement(By.TagName("p"));
+            IWebElement locatorcClass = driver.FindElement(By.ClassName("locator_class"));
+            IWebElement locatorLinkText = driver.FindElement(By.LinkText("myLocator(5)"));
+            IWebElement locatorPartialLinkText = driver.FindElement(By.PartialLinkText("Find my locator"));
+            IWebElement locatorCss = driver.FindElement(By.CssSelector("input[myname='selenium']"));
+            IWebElement locatorXpath = driver.FindElement(By.XPath("//*[@class='btn btn-2']"));
+
+            List<IWebElement> elementsList = new List<IWebElement>
+           {
+             locatorId,
+             locatorName,
+             locatorTag,
+             locatorcClass, 
+             locatorLinkText,
+             locatorPartialLinkText,
+             locatorCss,
+             locatorXpath
+           };
+
+            foreach (IWebElement element in elementsList)
+            {
+                Console.WriteLine(element);
+            }
+
+
+        }
+    }
+}
