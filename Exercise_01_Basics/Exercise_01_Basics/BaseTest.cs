606504d47debb186475472192e2da1cfcfb398c1
Gil Eliav <gil.eliav3@gmail.com>
2026-07-02 11:51:09 +0300

    feat: add initial project structure and exercise 1

---
+++ Exercise_01_Basics/Exercise_01_Basics/BaseTest.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise_01_Basics
+{
+    public class BaseTest
+    {
+        protected IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver=new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://www.imdb.com/");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+           if (driver != null)
+            {
+                driver.Quit();
+            }
+}
+}
+}
